71e19487604192500d9f4491018e13ec60389f03
keito ohashi <keitoohashi602@gmail.com>
2020-05-16 12:37:54 +0900

    共通関数を作成

---
+++ src/Utils/AppUtility.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Utils;
+
+/**
+ * AppUtility.
+ */
+class AppUtility
+{
+    /*
+     * function 
+     */
+    public static function add($val1, $val2){
+        return ($val1 + $val2);
+    }
+}
