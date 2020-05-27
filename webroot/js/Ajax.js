d8cd1d0e499c7e403c5df7719ab1af0571d53b4d
keito ohashi <keitoohashi602@gmail.com>
2020-05-27 22:42:22 +0900

    本の画像を表示

--- webroot/js/Ajax.js
+++ webroot/js/Ajax.js
@@ -1,10 +1,10 @@
 $(function () {
-    $("#form").on('click', function () {
+    $("#submit").on('click', function () {
 
-        var form = $("#form");
+        var form = $(this).parents("Form");
         var param = form.serializeArray();
-        console.log(param[1]["value"]);
-        var base_url = "/ShareRee/Books/ajaxTest";
+        console.log(param);
+        var base_url = "/ShareRee/Books/test";
         $.ajax({
             url: base_url, //送り先のコントローラーのURL
             type: "POST", //post通信
@@ -12,8 +12,11 @@ $(function () {
             data: {'params':param[1]["value"]}
         }).done(function (data) {
             // 通信成功時の処理
-            alert(data.result);
-            console.log(data);
+            var count=data.length;
+            for(var i=0;i<count;i++){
+                $('#text').append('<img src='+data[i]+' >');
+                console.log(data[i]);
+            }
     }).fail(function (jqXHR, textStatus, errorThrown) {
             // 通信失敗時の処理
             alert('ファイルの取得に失敗しました。');
@@ -22,5 +25,6 @@ $(function () {
             console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
             console.log("errorThrown    : " + errorThrown.message); // 例外情報
     });
-    })
+    return false;//ページを更新しないようにする
+    });
 });
