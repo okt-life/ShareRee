c4f0424e9e06549a45902874c18c9d6c03169000
keito ohashi <keitoohashi602@gmail.com>
2020-06-02 06:54:46 +0900

    ajax

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -10,26 +10,25 @@ class BooksController extends AppController
 
     public function index()
     {
+        /* $this->setAction('test');  */
     }
 
     public function test()
     {
-        $this->autoRender = false; //自動レンダリングをオフ
+        $this->autoRender=false;
         if ($this->request->is('ajax')) {
             $data = $this->request->getData();
             $results = AppUtility::add($data["params"]);
             $results = json_decode($results);
             $books = $results->items;
-            $thumbnail = [];
             $total_count = $results->totalItems;
             $get_count = count($books);
             foreach ($books as $book) {
                 // サムネ画像
-                $thumbnail[] = $book->volumeInfo->imageLinks->thumbnail;
-                $this->log($thumbnail);
+                $thumbnails[] = $book->volumeInfo->imageLinks->thumbnail;
             }
-            $this->log($get_count);
-            $this->response->body(json_encode($thumbnail));
+            $this->log($thumbnails);
+            $this->response->body(json_encode($thumbnails));
         }
     }
 }
--- webroot/js/Ajax.js
+++ webroot/js/Ajax.js
@@ -1,6 +1,5 @@
 $(function () {
     $("#submit").on('click', function () {
-
         var form = $(this).parents("Form");
         var param = form.serializeArray();
         console.log(param);
@@ -8,23 +7,31 @@ $(function () {
         $.ajax({
             url: base_url, //送り先のコントローラーのURL
             type: "POST", //post通信
-            dataType: "json", 
-            data: {'params':param[1]["value"]}
-        }).done(function (data) {
-            // 通信成功時の処理
-            var count=data.length;
-            for(var i=0;i<count;i++){
-                $('#text').append('<img src='+data[i]+' >');
-                console.log(data[i]);
+            dataType: "json",
+            data: {
+                'params': param[1]["value"]
+            },
+            beforeSend: function () {
+                $('.loading').removeClass('hide');
             }
-    }).fail(function (jqXHR, textStatus, errorThrown) {
+        }).done(function (data) {
+            $('#book-img').empty();
+            $('.loading').addClass('hide');
+            console.log(data);
+            // 通信成功時の処理
+            var count = data.length;
+            for (var i = 0; i < count; i++) {
+                $('#book-img').append('<a href="#"><img src=' + data[i] + '></a>');
+            }
+        }).fail(function (jqXHR, textStatus, errorThrown) {
             // 通信失敗時の処理
             alert('ファイルの取得に失敗しました。');
+            $('.loading').addClass('hide');
             console.log("ajax通信に失敗しました");
             console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
-            console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
+            console.log("textStatus     : " + textStatus); // タイムアウト、パースエラー
             console.log("errorThrown    : " + errorThrown.message); // 例外情報
-    });
-    return false;//ページを更新しないようにする
+        });
+        return false;
     });
 });
